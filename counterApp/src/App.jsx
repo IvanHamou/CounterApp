2a25b2a189b24cc26edadfebf532e0723a4b3fe4
Ivan Hamou <ivanhamou@gmail.com>
2024-11-17 10:40:21 +0100

    added functions to setCounter value

--- counterApp/src/App.jsx
+++ counterApp/src/App.jsx
@@ -3,12 +3,20 @@ import Counter from './components/Counter'
 import SetCounter from './components/SetCounter'
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `Count: ${count}`
+  }, [count])
+
+  console.log(count);
+  
 
   return (
     <div>
       <h1>Simple Counter App</h1>
       <Counter/>
-      <SetCounter/>
+      <SetCounter setCount={setCount}/>
     </div>
   )
 }
--- counterApp/src/components/SetCounter.jsx
+++ counterApp/src/components/SetCounter.jsx
@@ -1,10 +1,26 @@
-export default function SetCounter({ setCount }) {
+import { useState } from 'react'
 
+export default function SetCounter({ setCount }) {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newCount = Number(inputValue)
+    
+    if (newCount !== 0 && !newCount) {
+      alert('Please enter a valid number')
+    } else {
+      setCount(newCount)
+      setInputValue('')
+    }
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
         placeholder="Set counter value"
       />
       <button type="submit">
